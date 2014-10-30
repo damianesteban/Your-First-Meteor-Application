cbae9a2bf1db7fe63a7b70137d611e642e322f72
Damian Esteban <damian.esteban@gmail.com>
2014-10-30 14:27:18 -0400

    add conditionals so our code will be execute where we want - the client or the server

--- leaderboard.js
+++ leaderboard.js
@@ -1,3 +1,19 @@
 // We don't use the 'var' keyword below, because we want to create
 // a global variable.
 PlayersList = new Mongo.Collection('players');
+
+// The code below display itself in BOTH the command line (server) AND
+// the client (look in the DevTools console) because it is running on both.
+console.log("Hello World from Meteor!");
+
+// Now if we add a conditional:
+if(Meteor.isClient){
+  // This code only runs on the client! (you'll see it in the DevTools console only!)
+  // console.log("Hello Client!");
+}
+
+// Now if we add another conditional:
+if(Meteor.isServer){
+  // This code only runs on the server! (you'll see it in the command line only!)
+  // console.log("Hello Server!");
+}
