cf18bbc733e509c3668b145c5a00e94cee2bad64
Damian Esteban <damian.esteban@gmail.com>
2014-10-30 14:51:40 -0400

    add example helper function

--- leaderboard.js
+++ leaderboard.js
@@ -10,6 +10,26 @@ console.log("Hello World from Meteor!");
 if(Meteor.isClient){
   // This code only runs on the client! (you'll see it in the DevTools console only!)
   // console.log("Hello Client!");
+  //=====================================================================
+  // The below is deprecated, but it gives us a good example of how helpers work.
+  // 1 - The Template keyword searches through the templates in our Meteor application
+  // 2 - The leaderboard keyword is a reference to the name of the template we created
+  // earlier.  Every helper function we create needs to be attached to an individual template.
+  // 3 - The player keyword is a name we're giving to this function.  You can choose whatever
+  // name you like but do know that we'll soon reference it from inside the HTML file.
+  // Template.leaderboard.player = function(){
+  //  return "some text from the helper function"
+  //   }
+  //=================================================================
+  // Instead, we create a block.  We use the JSON format in between the curly brackets.
+  Template.leaderboard.helpers({
+    'player': function(){
+        return "Some text from the player helper function"
+    },
+    'otherHelperFunction': function(){
+      return "Some text from some other help function"
+    }
+  });
 }
 
 // Now if we add another conditional:
